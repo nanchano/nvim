5bfd460e44e3f25d2339d9d56489f2ee8ecf2168
nanchano <nicolas.anchanio@gmail.com>
2023-09-07 10:56:05 -0300

    snippet and autocompletion support

--- init.lua
+++ init.lua
@@ -83,8 +83,13 @@ require('lazy').setup({
         -- Autocompletion
         'hrsh7th/nvim-cmp',
         dependencies = {
+            -- Snippet Engine & its associated nvim-cmp source
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
             -- Adds LSP completion capabilities
             'hrsh7th/cmp-nvim-lsp',
+            -- Adds a number of user-friendly snippets
+            'rafamadriz/friendly-snippets',
         },
     },
 
@@ -408,8 +413,16 @@ mason_lspconfig.setup_handlers {
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
 local cmp = require 'cmp'
+local luasnip = require 'luasnip'
+require('luasnip.loaders.from_vscode').lazy_load()
+luasnip.config.setup {}
 
 cmp.setup {
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
     mapping = cmp.mapping.preset.insert {
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-p>'] = cmp.mapping.select_prev_item(),
@@ -423,6 +436,8 @@ cmp.setup {
         ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
+            elseif luasnip.expand_or_locally_jumpable() then
+                luasnip.expand_or_jump()
             else
                 fallback()
             end
@@ -430,6 +445,8 @@ cmp.setup {
         ['<S-Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+                luasnip.jump(-1)
             else
                 fallback()
             end
@@ -437,5 +454,6 @@ cmp.setup {
     },
     sources = {
         { name = 'nvim_lsp' },
+        { name = 'luasnip' },
     },
 }
