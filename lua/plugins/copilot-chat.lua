b9fe4cc5256a56a8bf701ef61a0b48222f4ba7dc
nanchano <nicolas.anchanio@gmail.com>
2024-10-01 16:01:45 -0300

    copilot chat

---
+++ lua/plugins/copilot-chat.lua
@@ -0,0 +1,22 @@
+return {
+	"CopilotC-Nvim/CopilotChat.nvim",
+	branch = "canary",
+	dependencies = {
+		{ "github/copilot.vim" },
+		{ "nvim-lua/plenary.nvim" },
+	},
+	build = "make tiktoken",
+	opts = {
+		debug = true,
+		model = 'gpt-4o',
+		clear_chat_on_new_prompt = false,
+	},
+	config = function()
+		require("CopilotChat").setup()
+
+		vim.keymap.set("n", "<leader>cc", ":CopilotChatOpen<CR>", {})
+		vim.keymap.set("v", "<leader>cf", ":CopilotChatFix<CR>", {})
+		vim.keymap.set("v", "<leader>cd", ":CopilotChatDocs<CR>", {})
+		vim.keymap.set("v", "<leader>ct", ":CopilotChatTests<CR>", {})
+	end
+}
