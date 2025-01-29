cbc85d1cd416b4cad70fda8e4e59f09ab94c2ec7
nanchano <nicolas@whisper.sh>
2025-01-29 16:55:34 -0300

    codecompanion

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,21 @@
+return {
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		require("codecompanion").setup({
+			strategies = {
+				chat = {
+					adapter = "copilot",
+				},
+				inline = {
+					adapter = "copilot",
+				},
+			},
+		})
+
+		vim.keymap.set("n", "<leader>cc", ":CodeCompanionChat<CR>", {})
+	end,
+}
